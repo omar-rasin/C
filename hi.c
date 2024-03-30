4c328a662cef894e04e43274b8e050246e44aa3b
omar-rasin <omarasinyt@gmail.com>
2024-03-30 21:52:39 +0600

    Add initial files

---
+++ hi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+ // this is a comment
+
+/*
+    this
+    is 
+    a 
+    multi
+    line
+    comment
+*/  
+ 
+    printf("escape sequences are : \n");
+    printf("character\ncombination\nwith\na\nbackslash\n");
+    printf("The following numbers are all integers :\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"Force equals mass time acceleration\"- Issac Newton\n");
+    printf("\"time is money\"-Martin Luther King");
+    return 0;
+}
