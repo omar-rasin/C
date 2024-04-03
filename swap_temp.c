ac305d403970f052c2a913f2878f121b084dd868
omar-rasin <omarasinyt@gmail.com>
2024-04-04 02:00:55 +0600

    Update

---
+++ swap_temp.c
@@ -0,0 +1,8 @@
+/* Write a C program to swap two numbers using a temporary variable. */
+#include<stdio.h>
+int main(){
+   int x = 100;
+   int y = 200;
+    printf("After swapping the initial integers of 100,200 becomes %d , %d", y,x);
+    return 0;
+}
