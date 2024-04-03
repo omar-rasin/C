848e4c8aada717b86e97df525550b14935d85856
omar-rasin <omarasinyt@gmail.com>
2024-04-04 01:52:02 +0600

    Update

---
+++ area_of_triangle.c
@@ -0,0 +1,10 @@
+/* Write a C program to calculate the area of a triangle given its base and height. */
+#include<stdio.h>
+int main(){
+    int base,height;
+    base = 12;
+    height = 8;
+    printf("The area of the triangle is %d", base*height/2);
+    return 0;
+
+}
