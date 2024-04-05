d03e17747451e5f1cb8956501ab0a7f09023a185
omar-rasin <omarasinyt@gmail.com>
2024-04-05 20:17:23 +0600

    New Update

--- swap_without_temp.c
+++ swap_without_temp.c
@@ -3,14 +3,14 @@
  int main(){
 
     int x,y;
-    x = 5;
-    y = 4;
+    x = 100;
+    y = 200;
 
 
     x = x+y;
     y = x-y;
     x = x-y;
-    printf("After switching , the initial values of 5,4 are %d,%d", x,y);
+    printf("After switching , the values of x,y are %d,%d", x,y);
 
     return 0;
  }
