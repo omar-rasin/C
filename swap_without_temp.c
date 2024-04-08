8cb1ef095940c88b572c13c6e1195df81a6103fc
omar-rasin <omarasinyt@gmail.com>
2024-04-08 12:20:26 +0600

    Update

--- swap_without_temp.c
+++ swap_without_temp.c
@@ -1,15 +1,17 @@
 /* Write a C program to swap two numbers without using a temporary variable. */
 #include<stdio.h>
+
  int main(){
 
     int x,y;
+
     x = 100;
     y = 200;
-
 
     x = x+y;
     y = x-y;
     x = x-y;
+
     printf("After switching , the values of x,y are %d,%d", x,y);
 
     return 0;
