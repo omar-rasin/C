7806dfdcf243374f0a6a2b4ca704578133a80822
omar-rasin <omarasinyt@gmail.com>
2024-04-18 11:34:34 +0600

    Update

---
+++ 1_to_n_forloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int num;
+        printf("Enter a number: ");
+        scanf("%d", &num);
+
+        for(int i=1; i<=num; i++){
+            printf(" %d", i);
+        }
+
+    return 0;
+}
