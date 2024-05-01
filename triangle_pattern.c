84cbb7d9af4f9aaa5b3cc6ba1180883059347a85
omar-rasin <omarasinyt@gmail.com>
2024-05-01 23:38:59 +0600

    Update

---
+++ triangle_pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    int height;
+
+    scanf("%d", &height);
+
+    for(int i=1;i<=height;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
