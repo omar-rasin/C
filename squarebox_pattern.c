1c20d85260640a0b46c4e09ceeaa732a50ee4fd3
omar-rasin <omarasinyt@gmail.com>
2024-05-01 23:39:09 +0600

    Update

---
+++ squarebox_pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    int length;
+
+    scanf("%d", &length);
+
+    for(int i=1;i<=length;i++){
+            for(int j=1;j<=length;j++)
+            printf("*  ");
+        printf("\n");
+
+    }
+    return 0;
+}
