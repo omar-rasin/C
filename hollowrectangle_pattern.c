7dc830fa4e1f309eabd054170ab3108200a5e748
omar-rasin <omarasinyt@gmail.com>
2024-05-01 23:40:45 +0600

    Update

---
+++ hollowrectangle_pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+    int m,n;
+
+    scanf("%d", &m);
+    scanf("%d", &n);
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||i==(m-1)||j==0||j==(n-1)){
+                printf("*");
+            }else printf(" ");
+        }printf("\n");
+    }
+    return 0;
+}
