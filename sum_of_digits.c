ffd222651de8899660ac834a20b636e5a74065a9
omar-rasin <omarasinyt@gmail.com>
2024-05-05 23:49:15 +0600

    Update

---
+++ sum_of_digits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    int num;
+    int sum=0;
+    scanf("%d", &num);
+
+    while(num!=0){
+        sum=sum+(num%10);
+        num=num/10;
+    }
+    printf("Sum of digits are %d", sum);
+
+    return 0;
+}
