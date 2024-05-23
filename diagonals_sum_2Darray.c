0f7b217b750b472a8c64a65bc136ee4b6471d4b3
omar-rasin <omarasinyt@gmail.com>
2024-05-23 23:09:15 +0600

    Update

--- diagonals_sum_2Darray.c
+++ diagonals_sum_2Darray.c
@@ -23,18 +23,31 @@ int main() {
 
     int sum_diagonal1 = 0; //diagonal1 is where column and index value is same(y=x)
 
-    for(int i=0; i<rows && i<columns; i++){
-        sum_diagonal1+= array[i][i];
+    for(int i=0; i<rows; i++){
+
+        for (int j=0; j<i+1; j++) {
+
+            if(j==i) {
+
+                sum_diagonal1 += array[i][j];
+            }
+        }
     }
 
-    int sum_diagonal2 = 0; //diagonal 2 is where y=-x
+    //int sum_diagonal2 = 0; //diagonal 2 is where y=-x
 
-    for(int i=0; i<rows && (columns-i-1)>=0; i++){
-        sum_diagonal2 +=array[i][columns-1-i];
-    }
+    //for(int i=0; i<rows ; i++){
+
+        //for (int j=0; j<(rows-1-i); j++) {
+
+            
+        //}
+
+        
+    //}
 
     printf("Sum of diagonal 1 is: %d\n", sum_diagonal1);
-    printf("Sum of diagonal 2 is: %d", sum_diagonal2);
+    //printf("Sum of diagonal 2 is: %d", sum_diagonal2);
 
     return 0;
 
