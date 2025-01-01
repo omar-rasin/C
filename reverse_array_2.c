20b51f0a6ee8890745dfcf6f2e2c989c512a1b10
omar-rasin <omarasinyt@gmail.com>
2025-01-02 01:29:41 +0600

    reverse array method 2

---
+++ reverse_array_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int array[size];
+    int reverseArray[size];
+
+    for(int i=0;i <size;i++) {
+        printf("Enter the value at index[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+
+      int k = 0; 
+         for (int i = size - 1; i >= 0; i--) { 
+        for (int j = k; j < k + 1; j++) { 
+            reverseArray[j] = array[i];
+        }
+        k++; 
+    }
+
+
+    for(int i=0; i<size; i++) {
+
+        printf("%d\t", reverseArray[i]);
+    }
+
+    return 0;
+}
