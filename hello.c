b03007601a435e6e24ff53920ec6528d66d9766c
omar-rasin <omarasinyt@gmail.com>
2025-01-06 16:44:26 +0600

    "Folder re-structuring"

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    
-    int letter = 9;
-    printf("The letter is %d", letter);
-    return 0;
-}
--- output.c
+++
@@ -1,9 +0,0 @@
-/* Write a C program that prints "Hello, World!" to the console. */
-#include<stdio.h>
-
-int main(){
-    
-    printf("Hello World");
-    
-    return 0;
-}
